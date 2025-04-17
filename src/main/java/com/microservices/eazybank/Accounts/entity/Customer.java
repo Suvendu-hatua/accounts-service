82234c2c0d44586e98698af5daff931e7773b633
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-17 21:51:38 +0530

    Updated fields of Customer entity , and extending BaseEntity

--- src/main/java/com/microservices/eazybank/Accounts/entity/Customer.java
+++ src/main/java/com/microservices/eazybank/Accounts/entity/Customer.java
@@ -1,4 +1,23 @@
 package com.microservices.eazybank.Accounts.entity;
 
-public class Customer {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "customer")
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private long customerId;
+    private String name;
+    private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
 }
