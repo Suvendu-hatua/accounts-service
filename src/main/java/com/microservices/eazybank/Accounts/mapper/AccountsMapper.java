2a2fe644e8e9b515e78cc92ab1fcb91058dd22fc
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-17 22:40:49 +0530

    Added AccountMapper and CustomerMapper to mapper package

---
+++ src/main/java/com/microservices/eazybank/Accounts/mapper/AccountsMapper.java
@@ -0,0 +1,21 @@
+package com.microservices.eazybank.Accounts.mapper;
+
+import com.microservices.eazybank.Accounts.dto.AccountsDto;
+import com.microservices.eazybank.Accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+}
