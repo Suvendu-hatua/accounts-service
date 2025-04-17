c2ae8be28cb3e6360c8f043cb1a204ebc7b1391e
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-17 22:42:30 +0530

    Added CustomerAlreadyExistsException

---
+++ src/main/java/com/microservices/eazybank/Accounts/exception/CustomerAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.microservices.eazybank.Accounts.exception;
+
+public class CustomerAlreadyExistsException extends RuntimeException{
+
+    public CustomerAlreadyExistsException(String message) {
+        super(message);
+    }
+}
