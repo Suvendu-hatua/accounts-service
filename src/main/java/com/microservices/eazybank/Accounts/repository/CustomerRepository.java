5968b62a7ff21cc11073b5a73fbe986165c3ece0
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-20 12:56:09 +0530

    Updating repositories

--- src/main/java/com/microservices/eazybank/Accounts/repository/CustomerRepository.java
+++ src/main/java/com/microservices/eazybank/Accounts/repository/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.microservices.eazybank.Accounts.repository;
 import com.microservices.eazybank.Accounts.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
--- src/main/java/com/microservices/eazybank/Accounts/service/IAccountService.java
+++ src/main/java/com/microservices/eazybank/Accounts/service/IAccountService.java
@@ -1,4 +1,8 @@
 package com.microservices.eazybank.Accounts.service;
 
+import com.microservices.eazybank.Accounts.dto.CustomerDto;
+
 public interface IAccountService {
+
+    void createAccount(CustomerDto customerDto);
 }
