00235ebd7be497ba0dd15b84ef9317942d3b27a4
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-20 12:57:13 +0530

    Updating ResponseDto class

--- src/main/java/com/microservices/eazybank/Accounts/dto/ResponseDto.java
+++ src/main/java/com/microservices/eazybank/Accounts/dto/ResponseDto.java
@@ -1,8 +1,10 @@
 package com.microservices.eazybank.Accounts.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ResponseDto {
     private String statusCode;
     private String statusMsg;
