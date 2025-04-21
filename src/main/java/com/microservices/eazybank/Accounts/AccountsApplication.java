6375f5af2827f4d1c012be2b647245275e772027
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-21 23:50:30 +0530

    Adding API documentation for the application

--- src/main/java/com/microservices/eazybank/Accounts/AccountsApplication.java
+++ src/main/java/com/microservices/eazybank/Accounts/AccountsApplication.java
@@ -1,9 +1,34 @@
 package com.microservices.eazybank.Accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts Microservices Rest API Documentation",
+				description = "Banking: Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Suvendu Hatua",
+						email = "iamsuvenduhatua@gmail.com",
+						url = "www.google.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://localhost:8080/swagger-ui/index.html"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Banking: Accounts microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
