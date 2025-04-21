a0a7fa1cab9ab9b8d6e01a1daf7c4511843c0f80
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-04-21 23:48:13 +0530

    Updating AccountsRepository and implemented methods

--- src/main/java/com/microservices/eazybank/Accounts/controller/AccountsController.java
+++ src/main/java/com/microservices/eazybank/Accounts/controller/AccountsController.java
@@ -2,17 +2,25 @@ package com.microservices.eazybank.Accounts.controller;
 
 import com.microservices.eazybank.Accounts.constants.AccountsConstants;
 import com.microservices.eazybank.Accounts.dto.CustomerDto;
+import com.microservices.eazybank.Accounts.dto.ErrorResponseDto;
 import com.microservices.eazybank.Accounts.dto.ResponseDto;
 import com.microservices.eazybank.Accounts.service.IAccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts  Microservices",
+        description = "CRUD REST APIs to CREATE, UPDATE, FETCH AND DELETE account details"
+)
 @RestController
 @RequestMapping(path = "/api/v1/accounts")
 @Slf4j
@@ -25,6 +33,33 @@ public class AccountsController {
         this.accountService = accountService;
     }
 
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer &  Account in Banking"
+    )
+    @ApiResponses(
+            {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "HTTP Status CREATED",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "HTTP Status INTERNAL SERVER ERROR",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponseDto.class
+                                    )
+                            )
+                    )
+            }
+    )
+
     @PostMapping("/create")
     public ResponseEntity<ResponseDto>createAccount(@RequestBody CustomerDto customerDto) {
             accountService.createAccount(customerDto);
@@ -32,5 +67,36 @@ public class AccountsController {
             return ResponseEntity.status(HttpStatus.CREATED).body(
                     new ResponseDto(AccountsConstants.MESSAGE_201, AccountsConstants.MESSAGE_201)
             );
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto> fetchAccountsDetails(@RequestParam(required = false) String mobileNumber) {
+        CustomerDto customerDto = accountService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+        boolean status = accountService.updateAccount(customerDto);
+        if (status) {
+            log.info("Updated account successfully.");
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountsConstants.MESSAGE_200, AccountsConstants.MESSAGE_200));
+        }else{
+            log.info("Updated account failed.");
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(AccountsConstants.STATUS_417,AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam(required = false) String mobileNumber) {
+        boolean status = accountService.deleteAccount(mobileNumber);
+        if(status){
+            log.info("Deleted account successfully.");
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AccountsConstants.MESSAGE_200, AccountsConstants.MESSAGE_200));
+        }else{
+            log.info("Deleted account failed.");
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(AccountsConstants.STATUS_417,AccountsConstants.MESSAGE_417_DELETE));
+        }
+
     }
 }
--- src/main/java/com/microservices/eazybank/Accounts/repository/AccountsRepository.java
+++ src/main/java/com/microservices/eazybank/Accounts/repository/AccountsRepository.java
@@ -3,5 +3,11 @@ package com.microservices.eazybank.Accounts.repository;
 import com.microservices.eazybank.Accounts.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+
+    Optional<Accounts> findByCustomerId(long customerId);
+
+    void deleteByCustomerId(Long customerId);
 }
